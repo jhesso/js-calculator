1ac9943d331fbe74d21f5d8d2992868d6ed91cf5
Jhesso <juho.hesso@hotmail.com>
2022-06-13 03:17:48 +0300

    added functionality
    % button now works, +/- button now works, dividing by 0 now gives a snarky error. added keyboard support. added keyboard instructions with toggle button.

--- calculator.js
+++ calculator.js
@@ -3,8 +3,11 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalButton = document.querySelector('[data-equal]');
 const clearButton = document.querySelector('[data-clear]');
 const backspaceButton = document.querySelector('[data-backspace]');
+const changeSignButton = document.querySelector('[data-changeSign');
 const previousTextElement = document.querySelector('[data-previous');
 const currentTextElement = document.querySelector('[data-current');
+const instructionsButton = document.querySelector('[data-instructions-button]');
+const instructionText = document.querySelector('[data-instruction-text]');
 
 class Calculator {
 	constructor(previousTextElement, currentTextElement){
@@ -21,6 +24,12 @@ class Calculator {
 
 	backspace (){
 		this.currentOperand = this.currentOperand.toString().slice(0, -1);
+	}
+
+	changeSign (){
+		if (this.currentOperand != undefined){
+			this.currentOperand = this.currentOperand * -1;
+		}
 	}
 
 	addNumber (number){
@@ -40,6 +49,7 @@ class Calculator {
 		let computation;
 		const current = parseFloat(this.currentOperand);
 		const prev = parseFloat(this.previousOperand);
+		let error = false;
 
 		if (isNaN(current) || isNaN(prev)) return;
 		switch (this.operation) {
@@ -53,18 +63,28 @@ class Calculator {
 				computation = prev * current;
 				break;
 			case '÷':
-				computation = prev / current;
+				if (current == 0){
+					error = true;
+					break;
+				} else{
+					computation = prev / current;
+					break;
+				}
+			case '%':
+				computation = prev % current;
 				break;
 			default:
 				break;
 		}
 
-		this.currentOperand = computation;
+		if (error == false) this.currentOperand = Math.round((computation + Number.EPSILON) * 1000000) / 1000000;
+		else {this.currentOperand = computation; }
 		this.operation = undefined;
 		this.previousOperand = '';
 	}
 
 	getScreenNumber (number){
+		if (number == undefined) return;
 		const numberString = number.toString();
 		const integerPart = parseFloat(numberString.split('.')[0]);
 		const decimalPart = numberString.split('.')[1];
@@ -83,11 +103,23 @@ class Calculator {
 	}
 
 	updateScreen (){
-		this.currentTextElement.innerText = this.getScreenNumber(this.currentOperand);
-		if (this.operation != null){
-			this.previousTextElement.innerText = `${this.getScreenNumber(this.previousOperand)} ${this.operation}`;
-		} else {
+		if (this.currentOperand == undefined){
+			this.currentTextElement.innerText = '';
 			this.previousTextElement.innerText = '';
+			const screen = document.getElementById('screen');
+			const img = document.createElement('img');
+			img.src = './media/quick-math.gif';
+			img.style.width = '290px';
+			img.style.objectFit = 'contain';
+			screen.appendChild(img);
+			setTimeout( () => { screen.removeChild(img); this.clear(); }, 5000);
+		} else{
+			this.currentTextElement.innerText = this.getScreenNumber(this.currentOperand);
+			if (this.operation != null){
+				this.previousTextElement.innerText = `${this.getScreenNumber(this.previousOperand)} ${this.operation}`;
+			} else {
+				this.previousTextElement.innerText = '';
+			}
 		}
 	}
 }
@@ -122,3 +154,109 @@ backspaceButton.addEventListener('click', button => {
 	calculator.backspace();
 	calculator.updateScreen();
 })
+
+changeSignButton.addEventListener('click', button => {
+	calculator.changeSign();
+	calculator.updateScreen();
+})
+
+instructionText.style.display = 'none';
+
+instructionsButton.addEventListener('click', button => {
+	console.log(instructionText.style.display)
+	if (instructionText.style.display === "none"){
+		instructionText.style.display = "block";
+	} else{
+		instructionText.style.display = "none";
+	}
+})
+
+// Keyboard functionality:
+// there is probably a more elegant solution. this is just something quick i came up with
+document.addEventListener('keydown', (event) => {
+	let name = event.key;
+	switch (name) {
+		case '0':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '1':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '2':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '3':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '4':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '5':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '6':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '7':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '8':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '9':
+			calculator.addNumber(name);
+			calculator.updateScreen();
+			break;
+		case '+':
+			calculator.chooseOperation(name);
+			calculator.updateScreen();
+			break;
+		case '-':
+			calculator.chooseOperation(name);
+			calculator.updateScreen();
+			break;
+		case '*':
+			calculator.chooseOperation(name);
+			calculator.updateScreen();
+			break;
+		case '/':
+			calculator.chooseOperation('÷');
+			calculator.updateScreen();
+			break;
+		case '%':
+			calculator.chooseOperation(name);
+			calculator.updateScreen();
+			break;
+		case 'Escape':
+			calculator.clear();
+			calculator.updateScreen();
+			break;
+		case '=':
+			calculator.compute();
+			calculator.updateScreen();
+			break;
+		case 'Enter':
+			calculator.compute();
+			calculator.updateScreen();
+			break;
+		case 'Backspace':
+			calculator.backspace();
+			calculator.updateScreen();
+			break;
+		case '_':
+			calculator.changeSign();
+			calculator.updateScreen();
+			break;
+		default:
+			break;
+	}
+}, false);
